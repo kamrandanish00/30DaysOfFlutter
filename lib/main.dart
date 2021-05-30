b8b2cb3b9c31c80177a200437d658d1998a5a377
kamrandanish00 <kamrandanish00@gmail.com>
2021-05-30 14:50:32 +0500

    day 2 completed

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import './home_page.dart';
 
 void main() => runApp(MyApp());
 
@@ -6,22 +7,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('AppBar'),
-        ),
-        body: Center(
-          child: Container(
-            child: Text(
-              'Text inside Conatainer',
-              style: TextStyle(
-                fontSize: 34,
-                fontWeight: FontWeight.bold,
-              ),
-            ),
-          ),
-        ),
-      ),
+      home: HomePage(),
     );
   }
 }
