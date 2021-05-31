85dd1351604faad4354708b8e6815f5da5cca780
kamrandanish00 <kamrandanish00@gmail.com>
2021-05-31 07:11:08 +0500

    day 3 completed

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
-import './home_page.dart';
+import 'package:flutter_catalog/pages/login_page.dart';
+import 'pages/home_page.dart';
+import './pages/login_page.dart';
 
 void main() => runApp(MyApp());
 
@@ -7,7 +9,16 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: HomePage(),
+      // home: HomePage(),
+      themeMode: ThemeMode.light,
+      darkTheme: ThemeData(
+        brightness: Brightness.dark,
+      ),
+      initialRoute: "/LoginPage",
+      routes: {
+        "/": (context) => HomePage(),
+        "/LoginPage": (context) => LoginPage(),
+      },
     );
   }
 }
---
+++ lib/pages/home_page.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_catalog/pages/login_page.dart';
+
+class HomePage extends StatelessWidget {
+  final int days = 30;
+  final String name = 'Kamran Mehsood';
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Catalog App'),
+      ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Center(
+            child: Container(
+              child: Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Text(
+                  'Welcome to $days days of Flutter by $name.',
+                  style: TextStyle(
+                    fontSize: 22,
+                    fontWeight: FontWeight.bold,
+                  ),
+                  textAlign: TextAlign.center,
+                ),
+              ),
+            ),
+          ),
+          ElevatedButton(
+            onPressed: () => LoginPage,
+            child: Text('Login Page'),
+          ),
+        ],
+      ),
+      drawer: Drawer(),
+    );
+  }
+}
